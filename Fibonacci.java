cf70d89e6799e70ee451d58d5943d1f612abe8f6
iamShubhank <89256809+iamShubhank@users.noreply.github.com>
2021-10-09 18:40:06 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 public class Fibonacci {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    System.out.print("Enter position of number in fibonacci series that you want to print : ");
+    System.out.print("Enter position of number in fibonacci series you want to display : ");
     int n = scan.nextInt();
-    int fib = Fib(n-1);
-    System.out.println(fib);
+    int[] QB = new int[n+1];
+    int FibM = MemoizedFib(n-1, QB);
+    System.out.println(FibM);
   }
 
-  public static int Fib(int n) {
+  public static int MemoizedFib(int n, int []QB) {
     if (n == 0 || n == 1) {
       return n;
     }
-    int fibn1 = Fib(n - 1);
-    int fibn2 = Fib(n - 2);
+    if(QB[n] != 0) {
+      return QB[n];
+    }
+    int fibn1 = MemoizedFib(n - 1, QB);
+    int fibn2 = MemoizedFib(n - 2, QB);
     int fibn = fibn1 + fibn2;
     return fibn;
   }
